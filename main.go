39e8749a62b43fd74bd377c0e465f5e1225bdfee
Alina Lin <alin352@gatech.edu>
2025-11-19 15:29:16 -0500

    refactored code

--- main.go
+++ main.go
@@ -8,28 +8,19 @@ import (
 	"strconv"
 )
 
-func main() {
-	targetURL := "https://www.newegg.com/seagate-expansion-26tb-black-usb-3-0/p/N82E16822185116?Item=N82E16822185116&cm_sp=Homepage_SS-_-P2_22-185-116-_-11192025"
-	selector := "div.price-current"
+func ScrapePrice(targetURL string, selector string) (float64, error) {
 	fmt.Printf("attempting to GET %s\n", targetURL)
 
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", targetURL, nil)
 	if err != nil {
-		fmt.Printf("error creating HTTP request: %s\n", err)
-		return
+		return 0, fmt.Errorf("error creating HTTP request: %s\n", err)
 	}
 	
 	// set User-Agent header to mimic a browser
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
 	resp, err := client.Do(req)
 
-	// resp, err := http.Get(targetURL)
-
-	// if err != nil {
-	// 	fmt.Printf("error making GET request: %s\n", err)
-	// 	return
-	// }
 
 	// connection resources are released after the function finishes
 	defer resp.Body.Close()
@@ -42,16 +33,14 @@ func main() {
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	
 	if err != nil {
-		fmt.Printf("error loading HTTP response body into goquery: %s\n", err)
-		return
+		return 0, fmt.Errorf("error loading HTTP response body into goquery: %s\n", err)
 	}
 
 	// use selector to find element
 	selection := doc.Find(selector)
 	
 	if selection.Length() == 0 {
-		fmt.Printf("no elements found for selector: %s\n", selector)
-		return
+		return 0, fmt.Errorf("no elements found for selector: %s", selector)
 	}
 
 	// extract and print text content
@@ -64,28 +53,20 @@ func main() {
 	
 	price, err := strconv.ParseFloat(cleanedPriceText, 64)
 	if err != nil {
-		fmt.Printf("error parsing price text to float: %s\n", err)
+		return 0, fmt.Errorf("error parsing price text to float: %s", err)
+	}
+	return price, nil
+}
+
+func main() {
+	targetURL := "https://www.newegg.com/seagate-expansion-26tb-black-usb-3-0/p/N82E16822185116?Item=N82E16822185116&cm_sp=Homepage_SS-_-P2_22-185-116-_-11192025"
+	selector := "div.price-current"
+	price, err := ScrapePrice(targetURL, selector)
+	if err != nil {
+		fmt.Printf("Error scraping price: %s\n", err)
 		return
 	}
+	fmt.Printf("The scraped price is: $%.2f\n", price)
 	
-	fmt.Printf("extracted price: $%.2f\n", price)
 
-
-	// // read response body (html content)
-	// htmlBytes, err := io.ReadAll(resp.Body)
-
-	// if err != nil {
-	// 	fmt.Printf("error reading response body: %s\n", err)
-	// 	return
-	// }
-
-	// htmlContent := string(htmlBytes)
-	// fmt.Println("successfully fetched HTML content:")
-
-	// snippetLength := 500
-	// if len(htmlContent) > snippetLength {
-	// 	fmt.Println(htmlContent[:snippetLength] + "...")
-	// } else {
-	// 	fmt.Println(htmlContent)
-	// }
 }
